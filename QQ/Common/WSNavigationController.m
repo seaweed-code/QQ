61485a67a6c5032e5ba22103a6c7edcde0b9f8ad
weida-studio <weida_studio@163.com>
2015-08-13 21:59:49 +0800

    修改NavigationBar BkColor

--- QQ/Common/WSNavigationController.m
+++ QQ/Common/WSNavigationController.m
@@ -8,6 +8,11 @@
 
 #import "WSNavigationController.h"
 
+/**
+ *  @brief  获取当前系统版本号
+ */
+#define kCurrentVersion      ([[[UIDevice currentDevice]systemVersion]floatValue])
+
 #define kBkColorNavigaion   ([UIColor colorWithRed:0.204 green:0.722 blue:0.918 alpha:1])
 
 
@@ -17,9 +22,48 @@
 
 @implementation WSNavigationController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self SetupNavStyle];
     // Do any additional setup after loading the view.
+}
+
+
+-(void)SetupNavStyle
+{
+    UIColor *BkColor = kBkColorNavigaion;
+    if (kCurrentVersion < 7.0f)
+    {
+        [self.navigationBar setBackgroundImage:[[WSNavigationController CreateImageWithColor:BkColor] resizableImageWithCapInsets:UIEdgeInsetsMake(1, 1, 1, 1)] forBarMetrics:UIBarMetricsDefault];
+        if (kCurrentVersion > 6.f)
+        {
+            //去掉导航栏下边的投影
+            self.navigationBar.shadowImage = [[WSNavigationController CreateImageWithColor:[UIColor clearColor]] resizableImageWithCapInsets:UIEdgeInsetsMake(1, 1, 1, 1)];
+        }
+    }else
+    {
+        [self.navigationBar setBackgroundImage:[[WSNavigationController CreateImageWithColor:BkColor] resizableImageWithCapInsets:UIEdgeInsetsMake(5, 1, 5, 1)] forBarMetrics:UIBarMetricsDefault];
+        self.navigationBar.tintColor = [UIColor whiteColor];
+        //去掉导航栏下边的投影
+        self.navigationBar.shadowImage = [[WSNavigationController CreateImageWithColor:[UIColor clearColor]] resizableImageWithCapInsets:UIEdgeInsetsMake(1, 1, 1, 1)];
+    }
+    self.navigationBar.barStyle = UIBarStyleBlack;
+}
+
++ (UIImage *)CreateImageWithColor: (UIColor *) color
+{
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return theImage;
 }
 
 - (void)didReceiveMemoryWarning {
