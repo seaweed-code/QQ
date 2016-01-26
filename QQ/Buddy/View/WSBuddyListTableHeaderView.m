e5b1e2b3e93dc71ecf3fabf73048ac4d0158c74a
weida-studio <weida_studio@163.com>
2016-01-26 11:11:25 +0800

    增加联系人列表

--- QQ/Buddy/View/WSBuddyListTableHeaderView.m
+++ QQ/Buddy/View/WSBuddyListTableHeaderView.m
@@ -14,10 +14,6 @@
 {
     CALayer *_line;
     
-    CALayer *_image;
-    
-    UILabel *_groupName;
-    
     UILabel *_totalCount;
     
     UIButton *_button;
@@ -36,24 +32,22 @@
         _line = [[CALayer alloc]init];
         _line.backgroundColor = kBkColorLine.CGColor;
         [self.contentView.layer addSublayer:_line];
-     
-        _image = [[CALayer alloc]init];
-        _image.backgroundColor = [UIColor clearColor].CGColor;
-        _image.contents = (__bridge id _Nullable)([UIImage imageNamed:@"buddy_header_arrow"].CGImage);
-        [self.contentView.layer addSublayer:_image];
         
-        
-        _groupName = [[UILabel alloc]init];
-        _groupName.backgroundColor = [UIColor clearColor];
-        _groupName.font = [UIFont systemFontOfSize:14];
-        [self.contentView.layer addSublayer:_groupName.layer];
-
         _totalCount = [[UILabel alloc]init];
         _totalCount.backgroundColor = [UIColor clearColor];
+        _totalCount.textAlignment = NSTextAlignmentRight;
         _totalCount.font = [UIFont systemFontOfSize:10];
         [self.contentView.layer addSublayer:_totalCount.layer];
         
         _button = [[UIButton alloc]init];
+        _button.backgroundColor = [UIColor clearColor];
+        [_button setImage:[UIImage imageNamed:@"buddy_header_arrow"] forState:UIControlStateNormal];
+        [_button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        _button.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+        _button.imageEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
+        _button.titleEdgeInsets = UIEdgeInsetsMake(0, 15, 0, 30);
+        _button.titleLabel.font = [UIFont systemFontOfSize:13];
+        _button.imageView.contentMode =  UIViewContentModeScaleAspectFit;
         [_button addTarget:self action:@selector(showGroup:) forControlEvents:UIControlEventTouchUpInside];
         [self.contentView addSubview:_button];
     }
@@ -66,41 +60,30 @@
 {
     [super layoutSubviews];
     
-    _line.frame = CGRectMake(0, 0, self.bounds.size.width, 1);
+    _line.frame = CGRectMake(0, 0, self.bounds.size.width, 0.6);
     
-    CGFloat x     = 10;
-    CGFloat width = 7;
-    CGFloat height = 11;
-    _image.frame = CGRectMake(x,(self.bounds.size.height-height)/2,width,height);
-    
-    x += CGRectGetMaxX(_image.frame);
-    _groupName.frame = CGRectMake(x, 0, self.bounds.size.width/2 , self.bounds.size.height);
-    
-    width = 40;
-    _totalCount.frame = CGRectMake(self.bounds.size.width-width, 0,width, self.bounds.size.height);
+    CGFloat width = 40;
+    CGFloat Offset = 15;
+    _totalCount.frame = CGRectMake(self.bounds.size.width-width - Offset, 0,width, self.bounds.size.height);
     
     _button.frame = self.contentView.bounds;
-    
 }
 
--(void)prepareForReuse
-{
-    [super prepareForReuse];
-    
-    _image.transform = CATransform3DIdentity;//防止变形
-}
 
 #pragma mark - 事件处理
 
 -(void)showGroup:(UIButton *)sender
 {
-    sender.selected = !sender.selected;
+    static BOOL lll;
+    lll = !lll;
     [UIView animateWithDuration:0.5 animations:^
     {
-        if (sender.selected) {
-            _image.transform = CATransform3DMakeRotation(M_PI_2, 0, 0, 1);
-        }else {
-            _image.transform = CATransform3DIdentity;
+        if (lll)
+        {
+            _button.imageView.transform = CGAffineTransformMakeRotation(M_PI_2);
+        }else
+        {
+            _button.imageView.transform = CGAffineTransformIdentity;
         }
     }];
     
@@ -110,7 +93,9 @@
 {
     _sectionInfo = sectionInfo;
      _totalCount.text = [sectionInfo indexTitle];
-    _groupName.text = [sectionInfo name];
+    
+    [_button setTitle:[sectionInfo name] forState:UIControlStateNormal];
+
 }
 
 @end
