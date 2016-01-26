a9a872f73ea2d8c931ddfca305df0bc117de135b
weida-studio <weida_studio@163.com>
2016-01-26 11:18:48 +0800

    增加联系人列表

--- QQ/Buddy/View/WSBuddyListTableHeaderView.h
+++ QQ/Buddy/View/WSBuddyListTableHeaderView.h
@@ -4,7 +4,7 @@
 //
 //  Created by weida on 16/1/25.
 //  Copyright © 2016年 weida. All rights reserved.
-//
+//  https://github.com/weida-studio/QQ
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
